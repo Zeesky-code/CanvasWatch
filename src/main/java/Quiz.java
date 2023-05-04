00fb082fe9cc523eaf6edae84de274b663be1282
Zainab Lawal <zainablaw1012@gmail.com>
2023-05-04 16:05:29 +0300

    Created Quiz class to allow for getQuiz feature

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,48 @@
+public class Quiz {
+	private int id;
+	private String title;
+	private String instructions;
+	private String points;
+	private String dueDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	public String getPoints() {
+		return points;
+	}
+
+	public void setPoints(String points) {
+		this.points = points;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+}
+
